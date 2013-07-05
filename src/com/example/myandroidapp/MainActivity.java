3e5754faf1fe498f6a034d7b53ed0328419d5110
kdileep1990 <k.dileep.1990@gmail.com>
2013-07-05 16:38:47 +0530

    hi

--- src/com/example/myandroidapp/MainActivity.java
+++ src/com/example/myandroidapp/MainActivity.java
@@ -4,27 +4,52 @@ import org.apache.cordova.DroidGap;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.view.Menu;
 
 public class MainActivity extends /* Activity */DroidGap {
 
-    // @Override
-    // protected void onCreate(Bundle savedInstanceState) {
-    // super.onCreate(savedInstanceState);
-    // setContentView(R.layout.activity_main);
-    // }
+	// @Override
+	// protected void onCreate(Bundle savedInstanceState) {
+	// super.onCreate(savedInstanceState);
+	// setContentView(R.layout.activity_main);
+	// }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		if (checkScreenSize().equals("large")
+				|| checkScreenSize().equals("xlarge")) {
+			super.onCreate(savedInstanceState);
+			super.init();
+			super.loadUrl("file:///android_asset/www/index.html");
+		} else {
+			super.onCreate(savedInstanceState);
+			super.init();
+			super.loadUrl("file:///android_asset/www/index.html");
+		}
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	private String checkScreenSize() {
+		String screenSize;
+		if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+			screenSize = "xlarge";
+		} else if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE) {
+			screenSize = "large";
+		} else if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_NORMAL) {
+			screenSize = "normal";
+		} else if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_SMALL) {
+			screenSize = "small";
+		} else {
+			screenSize = "normal";
+		}
+		return screenSize;
+	}
 
-} 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
